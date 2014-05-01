6a743877b07dfa60f32d8362fa53230d373cac6d
Sean Manhertz <sean.manhertz@gmail.com>
2014-05-01 14:58:19 -0600

    Added more trivia and the monster ogre; now have to work on abilities.

--- main.js
+++ main.js
@@ -318,24 +318,76 @@ var marioOrigin = new Trivia("What was Mario's name in the original Donkey Kong 
 
 var gameFIFA2001 = new Trivia("What unusual feature did the game 'FIFA 2001' implement?",5,['A Scratch and Sniff CD',true],[['You could use other CD-ROMs to generate players',false],['It had a mode to play as the coaches and referees',false],['It would lock the player out during half-time and encourage them to go outside',false]]);
 
+var xboxBestSelling = new Trivia("What game is the best selling product of the original XBox?",2,['Halo 2',true],[['Halo 3',false],['Call of Duty:Modern Warfare',false],['Blinx the Time Sweeper',false]]);
 
+var  dynastyWarriors= new Trivia("What genre was the original Dynasty Warrior video game on Playstation?",3,['Fighting',true],[['Beat-Em Up',false],['Strategy',false],['Platformer',false]]);
+
+var  nintendoBaseballOwnership= new Trivia("What major league baseball team does Nintendo of America hold a majority share in?",4,['Seattle Mariners',true],[['Miami Marlins',false],['New York Yankees',false],['Colorado Rockies',false]]);
+
+var  capcomName= new Trivia("What is Capcom short for?",3,['Capsule Computers',true],[['Capsule Computating',false],['Capture Command',false],['Capital Computations',false]]);
+
+var ukBestSeller = new Trivia("What game is the best selling game for the Playstation 1 in the UK?",3,['Rayman',true],[['Tomb Raider',false],['Final Fantasy IX',false],['Crash Bandicoot',false]]);
+
+var  gamecubeBestSeller= new Trivia("What is the best selling game of the Nintendo GameCube?",3,['Super Smash Bros. Melee',true],[['Super Mario Sunshine',false],['Metroid Prime 2',false],['The Legend of Zelda: Wind Waker',false]]);
+
+var  forzaTickets= new Trivia("How many speeding tickets did the development team of the video game Forza Motorsport 2 collectively gain?",6,['41',true],[['42',false],['10',false],['100',false]]);
+
+var  freeComicBookDay= new Trivia("What day is 'Free Comic Book Day' traditionally held in the United States?",1,['The first Saturday of May',true],[['May 4th',false],['The last Saturday of May',false],['December 28th, The birthday of Stan "The Man" Lee',false]]);
+
+var  fontComicSans= new Trivia("What comic book is the font Comic Sans derived from?",3,['Watchmen',true],[['The Dark Knight Returns',false],['Crisis of Infinite Earths',false],['Teenage Mutant Ninja Turtles',false]]);
+
+var  marvelMultiverse= new Trivia("According to the Marvel Comic's Multiverse, our reality is designated: ",5,['Earth-1218',true],[['Earth-616',false],['Earth-199999',false],['Earth-1610',false]]);
+
+var  weaponX= new Trivia("The X-men character Wolverine is a product of the infamous 'Weapon _' project. Which of the following heroes is also considered a product of this program?",0,['Captain America',true],[['Spider-Man',false],['The Punisher',false],['Colossus',false]]);
+
+var  muhammedAli= new Trivia("Boxer Muhammad Ali retired with the following record: ",3,['55 wins, 5 losses',true],[['50 wins, 10 losses',false],['42 wins, 3 losses',false],['50 wins, 6 losses',false]]);
+
+var  firstSuperBowl= new Trivia("The first Super Bowl was played in 1967. What two teams played against each other?",4,['Green Bay Packers and Kansas City Chiefs',true],[['New York Giants and New England Patriots',false],['Pittsburgh Steelers and Oakland Raiders',false],['Buffalo Bills and Miami Dolphins',false]]);
+
+var  olympics2012= new Trivia("What city did the 2012 Summer Olympics take place in?",2,['London',true],[['Toronto',false],['Sochi',false],['Dublin',false]]);
+
+var  tigerWoodsAge= new Trivia("How old was golfer Tiger Woods the first time that he won the Masters?",3,['21',true],[['22',false],['18',false],['25',false]]);
+
+var  olympicSeparation= new Trivia("What year were the Winter and Summer Olympics separated and started to be held on alternating 2 year cycles?",6,['1994',true],[['1990',false],['1978',false],['1986',false]]);
+
+var  nameAvengers= new Trivia("In the comic books, which character came up with the name 'Avengers'",2,['Wasp',true],[['Captain America',false],['The Hulk',false],['Loki',false]]);
+
+var  cameoFrozen= new Trivia("What Disney couple made a cameo appearance in the movie Frozen?",4,['Rapunzel and Flynn',true],[['Ariel and Eric',false],['Snow White and Prince Charming',false],['Pocahontas and John Smith',false]]);
+
+var  genieActor= new Trivia("What famous actor provided the voice of Genie in Disney's 'Aladdin'?",1,['Robin Williams',true],[['Sean Connery',false],['Chris Hemsworth',false],['Danny DeVito',false]]);
+
+var  disneyLastFilm= new Trivia("What was the last movie personally overseen by Walt Disney?",5,['The Jungle Book',true],[['Sleeping Beauty',false],['Toy Story',false],['The Sword in the Stone',false]]);
+
+var  disneyDalmations= new Trivia("How many dalmation puppies are there?",1,['101',true],[['103',false],['1',false],['None any more, Cruella De Vil got all of them',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+// var  = new Trivia("",0,['',true],[['',false],['',false],['',false]]);
+
+// Trivia question sources: GamesRadar, didyouknowcomics.tumblr.com
 	// 					-Categories Definition-
-var disney = new Category('Disney Trivia',[donaldDuckName,aladdinTiger]);
-var videoGames = new Category('Video Game Odd Facts',[pacmanScore,marioOrigin,gameFIFA2001]);
-	
+var disney = new Category('Disney Trivia',[donaldDuckName,aladdinTiger,disneyDalmations,disneyLastFilm,genieActor,cameoFrozen]);
+var videoGames = new Category('Video Game Odd Facts',[pacmanScore,marioOrigin,gameFIFA2001, forzaTickets, gamecubeBestSeller, ukBestSeller, xboxBestSelling, dynastyWarriors, capcomName, nintendoBaseballOwnership]);
+var comicbooks = new Category('Comic Books',[nameAvengers,weaponX,marvelMultiverse,freeComicBookDay,fontComicSans]);	
+var sports = new Category('Sports',[olympics2012,nintendoBaseballOwnership,muhammedAli,gameFIFA2001,firstSuperBowl,tigerWoodsAge]);
+
 	// 					-Monster Ranks-
 var mook = new Rank('Mook');
 var elite = new Rank('Elite');
+var boss = new Rank('Boss');
 	// 					-Monster Definition-
 
 
 var goblin = new Monsters('Goblin',[disney],2,mook,1);
 var troll = new Monsters('Troll',[disney,videoGames],5,elite,3);
+var ogre = new Monsters('Ogre',[disney,videoGames,comicbooks,sports],7,boss,5)
 
 
 
 	// 					-Monster Pool-
-	var firstLevel = [goblin,troll];
+	var firstLevel = [goblin,troll, ogre];
 
 // -------------------------DOM Creation------------------
 
@@ -552,7 +604,8 @@ $(document).on('ready', function() {
 		placeArea.empty();
 	goblin.health = 2;
 	troll.health = 5;
-		firstLevel=[troll,goblin];
+	ogre.health = 7;
+		firstLevel=[troll,goblin,ogre];
 
 		$('.generate').hide();
 		$('.reset-game').hide();
