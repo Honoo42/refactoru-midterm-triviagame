d9bff7c28e8f4c0b6a0cea813d7f900c7c3bba84
Sean Manhertz <sean.manhertz@gmail.com>
2014-05-09 13:44:20 -0600

    Heroku Implementation trial 2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var _ = require('underscore');
 // var bootstrap = require('twitter-bootstrap');
 
 var app = express();
+var port = process.env.PORT || 7563
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +12,6 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var server = app.listen(7563, function() {
+var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
